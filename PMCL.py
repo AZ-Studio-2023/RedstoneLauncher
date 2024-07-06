d3b4bc39a80486654b2e9c4727afd45ba65bab7a
mstouk57g <KissLmisu@outlook.com>
2024-07-06 08:18:46 +0800

    加了一堆页面

--- PMCL.py
+++ PMCL.py
@@ -1,76 +1,10 @@
 # coding:utf-8
 import sys
 
-from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtGui import QIcon, QDesktopServices
-from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout
-from qfluentwidgets import (NavigationItemPosition, MessageBox, setTheme, Theme, SplitFluentWindow,
-                            NavigationAvatarWidget, qrouter, SubtitleLabel, setFont)
-from qfluentwidgets import FluentIcon as FIF
-
-
-class Widget(QFrame):
-
-    def __init__(self, text: str, parent=None):
-        super().__init__(parent=parent)
-        self.label = SubtitleLabel(text, self)
-        self.hBoxLayout = QHBoxLayout(self)
-
-        setFont(self.label, 24)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
-        self.setObjectName(text.replace(' ', '-'))
-
-        # !IMPORTANT: leave some space for title bar
-        self.hBoxLayout.setContentsMargins(0, 32, 0, 0)
-
-
-class Window(SplitFluentWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        # create sub interface
-        self.homeInterface = Widget('Home Interface', self)
-        self.musicInterface = Widget('Music Interface', self)
-        self.videoInterface = Widget('Video Interface', self)
-        self.folderInterface = Widget('Folder Interface', self)
-        self.settingInterface = Widget('Setting Interface', self)
-        self.albumInterface = Widget('Album Interface', self)
-        self.albumInterface1 = Widget('Album Interface 1', self)
-        self.albumInterface2 = Widget('Album Interface 2', self)
-        self.albumInterface1_1 = Widget('Album Interface 1-1', self)
-
-        self.initNavigation()
-        self.initWindow()
-
-    def initNavigation(self):
-        self.addSubInterface(self.homeInterface, FIF.HOME, 'Home')
-        self.addSubInterface(self.musicInterface, FIF.MUSIC, 'Music library')
-        self.addSubInterface(self.videoInterface, FIF.VIDEO, 'Video library')
-
-        self.navigationInterface.addSeparator()
-
-        self.addSubInterface(self.albumInterface, FIF.ALBUM, 'Albums', NavigationItemPosition.SCROLL)
-        self.addSubInterface(self.albumInterface1, FIF.ALBUM, 'Album 1', parent=self.albumInterface)
-        self.addSubInterface(self.albumInterface1_1, FIF.ALBUM, 'Album 1.1', parent=self.albumInterface1)
-        self.addSubInterface(self.albumInterface2, FIF.ALBUM, 'Album 2', parent=self.albumInterface)
-        self.addSubInterface(self.folderInterface, FIF.FOLDER, 'Folder library', NavigationItemPosition.SCROLL)
-
-
-        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
-
-        # NOTE: enable acrylic effect
-        # self.navigationInterface.setAcrylicEnabled(True)
-
-    def initWindow(self):
-        self.resize(900, 700)
-        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
-        self.setWindowTitle('PyQt-Fluent-Widgets')
-
-        desktop = QApplication.desktop().availableGeometry()
-        w, h = desktop.width(), desktop.height()
-        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from qfluentwidgets import setTheme, Theme
+from Views.main import Window
 
 if __name__ == '__main__':
     QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
---
+++ Views/main.py
@@ -0,0 +1,57 @@
+# coding:utf-8
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout
+from qfluentwidgets import NavigationItemPosition,  SplitFluentWindow, SubtitleLabel, setFont
+from qfluentwidgets import FluentIcon as FIF
+
+from Interfaces.MainInterface import MainInterface
+from Interfaces.VersionListsInterface import VersionListInterface
+
+class Widget(QFrame):
+
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent=parent)
+        self.label = SubtitleLabel(text, self)
+        self.hBoxLayout = QHBoxLayout(self)
+
+        setFont(self.label, 24)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+        self.setObjectName(text.replace(' ', '-'))
+
+        # !IMPORTANT: leave some space for title bar
+        self.hBoxLayout.setContentsMargins(0, 32, 0, 0)
+
+
+class Window(SplitFluentWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        # create sub interface
+        self.HomeInterface = MainInterface()
+        self.VersionsListInterface = VersionListInterface()
+        self.SettingsInterface = Widget('Video Interface', self)
+
+        self.initNavigation()
+        self.initWindow()
+
+    def initNavigation(self):
+        self.addSubInterface(self.HomeInterface, FIF.HOME, 'Home')
+        self.navigationInterface.addSeparator()
+        self.addSubInterface(self.VersionsListInterface, FIF.BOOK_SHELF, 'Version List')
+        #self.addSubInterface(self.SettingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+
+        # NOTE: enable acrylic effect
+        # self.navigationInterface.setAcrylicEnabled(True)
+
+    def initWindow(self):
+        self.resize(1600, 900)
+        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
+        self.setWindowTitle('PyQt-Fluent-Widgets')
+
+        desktop = QApplication.desktop().availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
