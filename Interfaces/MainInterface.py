89bc8646428377c8dc92995c669c6812bc415430
zlk-sys <18079031125@163.com>
2024-07-07 12:03:29 +0800

    添加资源

--- Interfaces/MainInterface.py
+++ Interfaces/MainInterface.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 from PyQt5.QtWidgets import QWidget
-
-from qfluentwidgets import SwitchButton
+from helper.getValue import MINECRAFT_ICON, FORGE_ICON, FABRIC_ICON
+from qfluentwidgets import SwitchButton, SplitPushButton, FluentIcon, Action, RoundMenu, VBoxLayout
+from PyQt5.QtGui import QIcon
 
 
 class MainInterface(QWidget):
@@ -9,10 +10,16 @@ class MainInterface(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setObjectName("MainInterface")
-        #     self.switchButton = SwitchButton(parent=self)
-        #     self.switchButton.move(48, 24)
-        #     self.switchButton.checkedChanged.connect(self.onCheckedChanged)
-        #
-        # def onCheckedChanged(self, isChecked: bool):
-        #     text = 'On' if isChecked else 'Off'
-        #     self.switchButton.setText(text)
+        self.mainLayout = VBoxLayout(self)
+        self.start_button = SplitPushButton(FluentIcon.PLAY, '选择版本并启动')
+        self.start_button.resize(200,100)
+        self.menu = RoundMenu(parent=self.start_button)
+        self.menu.addAction(Action(QIcon(MINECRAFT_ICON), '1.19', triggered=lambda: self.setGameInfo(MINECRAFT_ICON, "1.19")))
+        self.menu.addAction(Action(QIcon(FORGE_ICON), '1.19 Forge', triggered=lambda: self.setGameInfo(FORGE_ICON, "1.19 Forge")))
+        self.menu.addAction(Action(QIcon(FABRIC_ICON), '1.19 Fabric', triggered=lambda: self.setGameInfo(FABRIC_ICON, "1.19 Fabric")))
+        self.start_button.setFlyout(self.menu)
+        self.mainLayout.addWidget(self.start_button)
+
+    def setGameInfo(self, icon, version):
+        self.start_button.setIcon(QIcon(icon))
+        self.start_button.setText(str(version))
---
+++ helper/getValue.py
@@ -0,0 +1,3 @@
+MINECRAFT_ICON = "resource/image/minecraft.png"
+FORGE_ICON = "resource/image/forge.png"
+FABRIC_ICON = "resource/image/fabric.png"
