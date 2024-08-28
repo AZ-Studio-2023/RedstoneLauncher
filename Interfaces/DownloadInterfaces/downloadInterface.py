e9fdaba62e6eefa2f77fd38da8927ef1039ac597
zlk-sys <18079031125@163.com>
2024-08-28 11:36:38 +0800

    把下载先给去了

---
+++ Interfaces/DownloadInterfaces/downloadInterface.py
@@ -0,0 +1,128 @@
+# coding:utf-8
+import sys
+
+from PyQt5.QtCore import Qt, pyqtSignal, QEasingCurve, QUrl
+from PyQt5.QtGui import QIcon, QDesktopServices
+from PyQt5.QtWidgets import QLabel, QHBoxLayout, QVBoxLayout, QApplication, QFrame, QWidget
+
+from qfluentwidgets import (NavigationBar, NavigationItemPosition, NavigationWidget, MessageBox,
+                            isDarkTheme, setTheme, Theme, setThemeColor, SearchLineEdit,
+                            PopUpAniStackedWidget, getFont)
+from qfluentwidgets import FluentIcon as FIF
+from qframelesswindow import FramelessWindow, TitleBar
+
+from Helpers.Config import cfg
+from Helpers.CustomControls import ListViewHelper
+from Helpers.StartHelper import getAllVersion
+from Helpers.styleHelper import style_path
+from Helpers.getValue import MINECRAFT_ICON, FORGE_ICON, FABRIC_ICON
+from Interfaces.DownloadInterfaces.choseInterface import choseInterface
+
+
+class StackedWidget(QFrame):
+    """ Stacked widget """
+
+    currentChanged = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.hBoxLayout = QHBoxLayout(self)
+        self.view = PopUpAniStackedWidget(self)
+
+        self.hBoxLayout.setContentsMargins(0, 0, 0, 0)
+        self.hBoxLayout.addWidget(self.view)
+
+        self.view.currentChanged.connect(self.currentChanged)
+
+    def addWidget(self, widget):
+        """ add widget to view """
+        self.view.addWidget(widget)
+
+    def widget(self, index: int):
+        return self.view.widget(index)
+
+    def setCurrentWidget(self, widget, popOut=False):
+        if not popOut:
+            self.view.setCurrentWidget(widget, duration=300)
+        else:
+            self.view.setCurrentWidget(
+                widget, True, False, 200, QEasingCurve.InQuad)
+
+    def setCurrentIndex(self, index, popOut=False):
+        self.setCurrentWidget(self.view.widget(index), popOut)
+
+
+class downloadInterface(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setObjectName("downloadInterface")
+
+        # use dark theme mode
+        # setTheme(Theme.DARK)
+
+        # change the theme color
+        # setThemeColor('#0078d4')
+
+        self.hBoxLayout = QHBoxLayout(self)
+        self.navigationBar = ListViewHelper(self)
+        self.stackWidget = StackedWidget(self)
+
+
+        # initialize layout
+        self.initLayout()
+
+        # add items to navigation interface
+        self.initNavigation()
+        self.setQss()
+
+    def initLayout(self):
+        self.hBoxLayout.addWidget(self.navigationBar)
+        self.hBoxLayout.addWidget(self.stackWidget)
+        self.hBoxLayout.setStretchFactor(self.stackWidget, 1)
+
+    def initNavigation(self):
+        self.addSubInterface(choseInterface(), QIcon(MINECRAFT_ICON), "Minecraft")
+
+        self.stackWidget.currentChanged.connect(self.onCurrentInterfaceChanged)
+
+        # hide the text of button when selected
+        # self.navigationBar.setSelectedTextVisible(False)
+
+        # adjust the font size of button
+        # self.navigationBar.setFont(getFont(12))
+
+    def addSubInterface(self, interface, icon, text: str, position=NavigationItemPosition.TOP, selectedIcon=None):
+        """ add sub interface """
+        self.stackWidget.addWidget(interface)
+        self.navigationBar.addItem(
+            routeKey=interface.objectName(),
+            icon=icon,
+            text=text,
+            onClick=lambda: self.switchTo(interface),
+            selectedIcon=selectedIcon,
+            position=position,
+        )
+
+    def setQss(self):
+        with open(style_path(), encoding='utf-8') as f:
+            self.setStyleSheet(f.read())
+
+    def switchTo(self, widget):
+        self.stackWidget.setCurrentWidget(widget)
+
+    def onCurrentInterfaceChanged(self, index):
+        widget = self.stackWidget.widget(index)
+        self.navigationBar.setCurrentItem(widget.objectName())
+
+    def showMessageBox(self):
+        w = MessageBox(
+            '支持作者🥰',
+            '个人开发不易，如果这个项目帮助到了您，可以考虑请作者喝一瓶快乐水🥤。您的支持就是作者开发和维护项目的动力🚀',
+            self
+        )
+        w.yesButton.setText('来啦老弟')
+        w.cancelButton.setText('下次一定')
+
+        if w.exec():
+            QDesktopServices.openUrl(QUrl("https://afdian.net/a/zhiyiYo"))
