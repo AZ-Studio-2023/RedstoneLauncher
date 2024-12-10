d247e7def916e54ec602da3d8e07cf02469a64b8
zlk-sys <18079031125@163.com>
2024-12-10 21:56:15 +0800

    下载逻辑（部分）

--- Helpers/downloadHelper.py
+++ Helpers/downloadHelper.py
@@ -1,11 +1,20 @@
+import os
+
 import aria2p
-from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot
+from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot, QThreadPool
 import requests
-from Helpers.getValue import RPC_PORT, getDownloadData
+
+from Helpers.Config import cfg
+from Helpers.getValue import RPC_PORT, getDownloadData, CACHE_PATH, setDownloadData
 import json
 
 from Helpers.outputHelper import logger
 
+def find_dict(dictionary_list, key, value):
+    for dictionary in dictionary_list:
+        if key in dictionary and dictionary[key] == value:
+            return dictionary
+    return None
 
 class WorkerSignals(QObject):
     progress = pyqtSignal(str)
@@ -44,3 +53,72 @@ def download(url, path):
     )
     event = aria2.add_uris([url], options={"dir": path})
     return event
+
+class downloadSignals(QObject):
+    progress = pyqtSignal(str)
+
+class downloadVersions(QRunnable):
+    def __init__(self, name, version, modsLoader=None, modsLoaderVersion=None):
+        super(downloadVersions, self).__init__()
+        self.signals = downloadSignals()
+        self.name = name
+        self.version = version
+        self.modsLoader = modsLoader
+        self.modsLoaderVersion = modsLoaderVersion
+
+    @pyqtSlot()
+    def run(self):
+        # 下载版本JSON
+        f = open(os.path.join(CACHE_PATH, "version_manifest.json"), "r")
+        data = json.loads(f.read())
+        f.close()
+        version_dic = find_dict(data["versions"], "id", self.version)
+        self.pool = QThreadPool()
+        url: str = version_dic["url"]
+        if cfg.source.value != "官方":
+            url.replace("https://piston-meta.mojang.com", "https://bmclapi2.bangbang93.com")
+        if not os.path.exists(os.path.join(cfg.gamePath.value, "versions", self.name)):
+            os.makedirs(os.path.join(cfg.gamePath.value, "versions", self.name))
+        setDownloadData({"url": url, "path": os.path.join(cfg.gamePath.value, "versions", self.name, f'{self.name}.json')})
+        self.runnable = downloadJson()
+        self.pool.start(self.runnable)
+        self.pool.waitForDone()
+
+        f = open(os.path.join(cfg.gamePath.value, "versions", self.name, f'{self.name}.json'), "r")
+        versionData = json.loads(f.read())
+        f.close()
+
+        # 下载游戏Jar
+        download(versionData["downloads"]["client"]["url"], os.path.join(cfg.gamePath.value, "versions", self.name, f'{self.name}.jar'))
+
+        # 下载依赖库
+        for lib in versionData["libraries"]:
+            if "artifact" in lib["downloads"] and "classifiers" not in lib["downloads"]:
+                url = lib["downloads"]["artifact"]["url"]
+                if cfg.source.value != "官方":
+                    url.replace("https://libraries.minecraft.net", "https://bmclapi2.bangbang93.com/maven")
+                dir_path = os.path.join(cfg.gamePath.value, "libraries", os.path.dirname(lib["downloads"]["artifact"]["path"]))
+                if not os.path.exists(dir_path):
+                    os.makedirs(dir_path)
+                download(url, os.path.join(cfg.gamePath.value, "libraries", lib["downloads"]["artifact"]["path"]))
+            elif "classifiers" in lib["downloads"]:
+                url = lib["downloads"]["artifact"]["url"]
+                if cfg.source.value != "官方":
+                    url.replace("https://libraries.minecraft.net", "https://bmclapi2.bangbang93.com/maven")
+                dir_path = os.path.join(cfg.gamePath.value, "libraries",
+                                        os.path.dirname(lib["downloads"]["artifact"]["path"]))
+                if not os.path.exists(dir_path):
+                    os.makedirs(dir_path)
+                download(url, os.path.join(cfg.gamePath.value, "libraries", lib["downloads"]["artifact"]["path"]))
+
+                for cl in lib["downloads"]["classifiers"].values():
+                    url = cl["url"]
+                    if cfg.source.value != "官方":
+                        url.replace("https://libraries.minecraft.net", "https://bmclapi2.bangbang93.com/maven")
+                    dir_path = os.path.join(cfg.gamePath.value, "libraries",
+                                            os.path.dirname(lib["path"]))
+                    if not os.path.exists(dir_path):
+                        os.makedirs(dir_path)
+                    download(url, os.path.join(cfg.gamePath.value, "libraries", lib["path"]))
+
+
